e3ff00f555d8a83298fbe0c1674211e6e73917ec
valvour <akshat98dubey@gmail.com>
2026-05-18 02:52:50 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'notealarm-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const ASSETS = ['/notealarm/', '/notealarm/index.html', '/notealarm/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
